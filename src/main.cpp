317a21a908117caaf590f2947568cbf0029f557b
João Gerliandro <joaogerliandro@gmail.com>
2023-09-30 15:04:15 -0300

    feat - adding the algortihm logic base
    Co-Authored-By: Mateus Daniel de Lima da Silva <81442147+mateusdanie@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,24 @@
 #include <entities.h>
 #include <util.h>
 
-void dfs_algorithm(Entities::Node *root, int64_t key)
+Entities::Node* dfs(Entities::Node *root, int64_t key)
 {
-    
+    // Search the start
+}
+
+void dfs_metrics(Entities::Node *root, int64_t key)
+{
+    // Calculate the metrics and print
+}
+
+Entities::Node* bfs(Entities::Node *root, int64_t key)
+{
+    // Search the start
+}
+
+void bfs_metrics(Entities::Node *root, int64_t key)
+{
+    // Calculate the metrics and print
 }
 
 int main()
@@ -12,12 +27,15 @@ int main()
     Util::GraphLoader graph_loader;
 
     Entities::Node *map_graph = nullptr;
+    Entities::Node *start_node = nullptr;
+    Entities::Node *end_node = nullptr;
 
     graph_loader.load_graph(std::string("USA-road-d.NY"));
 
     map_graph = &graph_loader.graph_nodes[0];
 
-    dfs_algorithm(map_graph, 4);
+    start_node = dfs(map_graph, 4);
+    dfs_metrics(start_node, 1);
 
     return EXIT_SUCCESS;
 }
