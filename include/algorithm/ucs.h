b2883ff1af74105c7f8b7bdcb62f35a170a010fe
Mateus Daniel de Lima da Silva <81442147+mateusdanie@users.noreply.github.com>
2023-10-11 22:17:06 -0300

    feature - Adding UCS logic

--- include/algorithm/ucs.h
+++ include/algorithm/ucs.h
@@ -11,6 +11,55 @@ namespace Algorithm
         public:
             static void ucs_algorithm(Node *first_node, uint64_t last_value)
             {
+                //A logica é a mesma do BFS quase
+                std::vector<Node *> *border = new std::vector<Node *>;
+
+                first_node->color = GREY;
+
+                border->push_back(first_node);
+
+                bool found = false;
+                
+                while (!(border->empty()))
+                {
+                    if((*border)[0]->id == last_value)
+                    {
+                        std::cout << "Encontrou !" << std::endl;
+                        break;
+                    }
+
+                    Node *first_node_border = (*border)[0];
+
+                    border->erase(border->begin());
+
+                    for(Node *other_node : first_node_border->neighbors)
+                    {
+                        found = other_node->id == last_value ? true : false;
+
+                        if(other_node->color == WHITE)
+                        {
+                            other_node->color = GREY;
+                            border->push_back(other_node);
+                        }
+
+                        if(found)
+                            break;
+                    }
+
+                    //antes de ir pro próximo no de border tem que ordenar pela distância do menor pro maior
+
+                    if(border->back()->id == first_node_fifo->neighbors.back()->id)
+                        first_node_fifo->color = BLACK;
+
+                    if(found)
+                    {
+                        std::cout << "Encontrou !" << std::endl;
+                        break;
+                    }
+                }
+
+                delete border; 
+            }
             }
     };
 }
